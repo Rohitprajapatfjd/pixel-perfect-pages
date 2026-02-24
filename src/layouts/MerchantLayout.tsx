631471da7f63f78c6765267b9032f42208b1596b
Rohitprjapatfz <99191276+Rohitprajapatfjd@users.noreply.github.com>
2026-02-24 18:38:52 +0530

    Add animated flip-slide transition for auth mode switch

--- src/layouts/MerchantLayout.tsx
+++ src/layouts/MerchantLayout.tsx
@@ -1,28 +1,15 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { AppSidebar } from "@/components/merchant/layouts/AppSidebar";
-import AdminTopbar from "@/components/admin/AdminTopbar";
-import { cn } from "@/lib/utils";
 
-const AdminLayout = () => {
-  const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
-
+const MerchantLayout = () => {
   return (
     <div className="min-h-screen bg-secondary">
-      <AppSidebar/>
-      <div
-        className={cn(
-          "flex flex-col transition-all duration-300",
-          sidebarCollapsed ? "ml-[68px]" : "ml-[250px]"
-        )}
-      >
-        <AdminTopbar />
-        <main className="flex-1 p-6">
-          <Outlet />
-        </main>
-      </div>
+      <AppSidebar />
+      <main className="ml-[250px] flex-1 p-6">
+        <Outlet />
+      </main>
     </div>
   );
 };
 
-export default AdminLayout;
+export default MerchantLayout;
